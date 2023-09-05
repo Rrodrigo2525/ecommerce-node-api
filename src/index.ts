cb8efe0b9a02ca5018ab68fec45d362a9122c5b6
Rodrigo França <rodrigolopes2005@hotmail.com>
2023-09-04 21:32:55 -0300

    Meu primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
+import { CategoriaMap } from "@modules/catalogo/mappers/categoria.map";
+import { DomainException } from "@shared/domain/domain.exception";
+import { readFile, writeFile } from "fs";
+import { RecuperarCategoriaProps } from "./modules/catalogo/domain/categoria/categoria.types";
+
+
+try {
+
+    /////////////////////////
+    //Criando uma Categoria//
+    /////////////////////////
+
+    let categoria: Categoria;
+    categoria = Categoria.criar({nome:'Banho'});
+    console.log(categoria);
+
+    /////////////////////////////
+    //Recuperando uma Categoria//
+    /////////////////////////////
+
+    let propsCategoria: RecuperarCategoriaProps = {
+        id:'5fac700e-2783-4682-99cf-0c9c1d9675b0',
+        nome: 'mesáa'
+    };
+    let categoria2: Categoria = Categoria.recuperar(propsCategoria);
+    console.log(categoria2);
+    console.log(categoria.equals(categoria2));
+
+    //////////////////////////////////////////////////////
+    //Persistinto e Recuperando em Arquivo - File System//
+    //////////////////////////////////////////////////////
+
+    let arrayCategorias = [];
+    arrayCategorias.push(categoria.toDTO());
+    arrayCategorias.push(categoria2.toDTO());
+
+   
+    writeFile('categorias.json', JSON.stringify(arrayCategorias), function(error:any){  
+        if (error) throw error;
+        console.log('Arquivo Salvo com Sucesso!');
+        readFile('categorias.json', (error, dadoGravadoArquivo) => {
+            if (error) throw error;
+            console.log('Leitura de Arquivo!');
+            let categoriasSalvas: [] = JSON.parse(dadoGravadoArquivo.toString());
+            categoriasSalvas.forEach(categoriaJSON => {
+                console.log(categoriaJSON);
+                console.log(CategoriaMap.toDomain(categoriaJSON));
+            })
+        })
+    });
+
+   
+
+
+}
+catch (error:any) {
+    if (error instanceof DomainException) {
+        console.log('Exceção de Domínio---------------------');
+        console.log(error.message);
+    }
+    else {
+        console.log('Outras Exceções ----------------------');
+        console.log(error.message);
+    }
+}
+finally {
+    console.log('Ação que deve ser executada em caso de sucesso e em caso de exceção');
+}
---
+++ src/modules/catalogo/domain/categoria/categoria.types.ts
@@ -0,0 +1,14 @@
+//Todos os atributos/ propriedades que uma categoria deve ter no sistema
+
+import { type } from "os";
+
+//Auxilia na criação de invariantes e modelos ricos
+interface ICategoria {
+    id?: string;
+    nome: string;
+}
+
+type CriarCategoriaProps = Omit<ICategoria, "id">;
+type RecuperarCategoriaProps = Required<ICategoria>;
+
+export {ICategoria, CriarCategoriaProps, RecuperarCategoriaProps}
